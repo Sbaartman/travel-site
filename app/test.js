2463ff8cb57f63323a4a8c064ad6daa24ad6a676
Shane <sg.baartman@gmail.com>
2023-02-02 11:50:41 +0200

    Adding package json files

---
+++ app/test.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+fs.writeFile(__dirname + "/index.html", "<h1>Html is great</h1>", function(error){
+    if (error){
+        return console.log(error);
+    }else{
+        return console.log('Yay!');
+    }
+
+})
