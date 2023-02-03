d6f4587bb51c881f010170c22bae1e9c57f611a1
Shane <sg.baartman@gmail.com>
2023-02-03 11:20:52 +0200

    responsive images

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css'
+
+if (module.hot) {
+  module.hot.accept()
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
+]
+
+module.exports = {
+  entry: './app/assets/scripts/App.js',
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  },
+  devServer: {
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+      }
+    ]
+  }
+}
