4d9c9f5c2e7e1b053b5c352b1998e71714ce450c
Shane <sg.baartman@gmail.com>
2023-02-03 15:39:12 +0200

    header and footer js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,11 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept()
